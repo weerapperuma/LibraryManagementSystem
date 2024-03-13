ae757429ace9d16e79a504fc618d2a1c3682c1dd
weerapperuma <weerapperumasahiru@gmail.com>
2024-03-14 01:21:29 +0530

    book show form done

--- src/main/java/lk/penguin/controller/Pane1Controller.java
+++ src/main/java/lk/penguin/controller/Pane1Controller.java
@@ -1,14 +1,33 @@
 package lk.penguin.controller;
 
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.layout.VBox;
 import lk.penguin.util.Navigation;
 
 public class Pane1Controller {
+    @FXML
+    private VBox fxSignUpVbox;
 
     @FXML
     void down(ActionEvent event) {
         Navigation.autoScrollToBottom();
     }
 
+    public void mainSearchButtonOnAction(ActionEvent actionEvent) {
+
+    }
+
+    public void btnExitOnAction(ActionEvent actionEvent) {
+        Platform.exit();
+        System.exit(0);
+    }
+    @FXML
+    void btnSignOptionOnAction(ActionEvent event) {
+        fxSignUpVbox.setVisible(true);
+    }
+    public void initialize(){
+        fxSignUpVbox.setVisible(false);
+    }
 }
